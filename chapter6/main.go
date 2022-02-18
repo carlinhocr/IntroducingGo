584ab967d4aeda05b44978aea07db6bf061d6e87
carlinhocr <carlos.rienzi@gmail.com>
2022-02-18 16:46:44 -0300

    with chapter 6

--- chapter6/main.go
+++ chapter6/main.go
@@ -37,6 +37,7 @@ func panicTest() {
 
 func zero(xPtr *int) { // a pointer to an int
 	*xPtr = 0
+	fmt.Println("zero xPtr address", &xPtr)
 }
 
 func factorial(x uint) uint {
@@ -44,6 +45,36 @@ func factorial(x uint) uint {
 		return 1
 	}
 	return x * factorial(x-1)
+}
+
+func suma(args []int) (total int) {
+	total = 0
+	for _, value := range args {
+		total += value
+	}
+	return
+}
+
+func half(x int) (int, bool) {
+	halved := x / 2
+	fmt.Println(halved % 2)
+	even := false
+	if halved%2 == 0 {
+		even = true
+	} else {
+		even = false
+	}
+	return halved, even
+}
+
+func max(args ...int) int {
+	maximum := 0
+	for _, v := range args {
+		if v > maximum {
+			maximum = v
+		}
+	}
+	return maximum
 }
 
 func main() {
@@ -65,4 +96,9 @@ func main() {
 	fmt.Println("X pointer before modifying", xpointer)
 	zero(&xpointer)
 	fmt.Println("X pointer after modifying", xpointer)
+	fmt.Println(("total of suma"), suma([]int{1, 2, 3}))
+	result, even := half(5)
+	fmt.Println("the half of 5, is:", result, "is it Even?", even)
+	maxlist := []int{1, 4, 56, 77, 99, 5, 44}
+	fmt.Println("Maximum of 1,4,56,77,99,5,44 is", max(maxlist...))
 }
